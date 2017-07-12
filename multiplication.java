7378db5ece892858cdaaf27bef8014723f36216c
keerthanajanani <keerthijanani66@gmail.com>
2017-07-12 15:01:09 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,18 @@
+# multiplication
+import java.io.*;
+import java.util.*;
+public class multiplication
+{
+public static void main(String args[])throws IOException
+{
+int i,j,n;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter the input:");
+n=Integer.parseInt(br.readLine());
+for(i=1;i<=10;i++)
+{
+System.out.println(n+" * "+i+" = +n*i);
+}
+}
+}
+
